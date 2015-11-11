0d1cd571e7f982fe3c0cb6cd32ff7be1bab3d562
Skybound1 <mgupta94@hotmail.co.uk>
2015-11-11 15:23:08 +0000

    Adds missing semicolon at the end of class declaration

--- I2Cdev.h
+++ I2Cdev.h
@@ -29,6 +29,6 @@ class I2Cdev {
         static bool writeWord(uint8_t devAddr, uint8_t regAddr, uint16_t data);
         static bool writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data);
         static bool writeWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t *data);
-}
+};
 
 #endif // _I2CDEV_H_
