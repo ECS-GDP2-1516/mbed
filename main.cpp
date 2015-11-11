513f089eaa69bdfef6f7de8221caf8a54ac07a2c
Skybound1 <mgupta94@hotmail.co.uk>
2015-11-11 15:58:21 +0000

    Modifies main.cpp to use the MPU6050 and I2Cdev classes

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include "mbed.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// The offsets of the gyro
+#define Gx_offset -1.50
+#define Gy_offset 0
+#define Gz_offset 0.80
+// The offsets of the accel
+#define Ax_offset -0.07
+#define Ay_offset 0.02
+#define Az_offset 0.14
+
+MPU6050 accelgyro;
 
 DigitalOut myled(LED1);
 Serial serial(p9, p10);
-I2C sensor(p28, p27);
-uint8_t address = 210;
 
 /*int main() {
-    while(1) {
-        myled = 1;
-        serial.printf("Hello world\n");
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
-    }
-}*/
-
-int main() {
     while(1) {
         char data[1];
         int ret;
@@ -24,5 +25,14 @@ int main() {
             if(ret == 0) 
                 printf("%d, %d\n", address, ret);
         }
+    }
+}*/
+
+int main() {
+    printf("Initializing I2C device.....\n");
+    accelgyro.initialize();
+    printf("Testing device connections....\n");
+    printf(accelgyro.testConnection() ? "MPU6050 connection successfule\n" : "MPU6050 connection failure\n");
+    while(1) {
     }
 }
