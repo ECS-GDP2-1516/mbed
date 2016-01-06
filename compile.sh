63bbe262e39b6714bc371d0dcde61fae0031979a
Emily Shepherd <emily@emilyshepherd.me>
2016-01-06 15:40:39 +0000

    IT'S ALIVE

--- compile.sh
+++ compile.sh
@@ -15,17 +15,25 @@ arm-none-eabi-g++ \
 -I ./targets/cmsis \
 -I ../fs/fat/ChaN \
 -Os -s -fno-common  \
--lc_nano -lrdimon --specs=nosys.specs -mthumb -mcpu=cortex-m0 \
+ --specs=nosys.specs -mthumb -mcpu=cortex-m0 \
 -D NDEBUG \
 ../fs/fat/FATFileSystem.cpp \
 ../fs/fat/FATFileHandle.cpp \
+common/retarget.cpp \
 common/I2C.cpp \
 common/SerialBase.cpp \
 common/Serial.cpp \
 common/Stream.cpp \
 common/FileBase.cpp \
 common/FileLike.cpp \
+common/FilePath.cpp \
 common/gpio.c \
+common/board.c \
+common/wait_api.c \
+common/semihost_api.c \
+common/mbed_interface.c \
+common/us_ticker_api.c \
+common/ticker_api.c \
 ../../main.cpp \
 ../../classify.cpp \
 ../../I2Cdev.cpp \
@@ -34,8 +42,10 @@ targets/hal/TARGET_NXP/TARGET_LPC11UXX/serial_api.c \
 targets/hal/TARGET_NXP/TARGET_LPC11UXX/gpio_api.c \
 targets/hal/TARGET_NXP/TARGET_LPC11UXX/i2c_api.c \
 targets/hal/TARGET_NXP/TARGET_LPC11UXX/pinmap.c \
+targets/hal/TARGET_NXP/TARGET_LPC11UXX/us_ticker.c \
 common/pinmap_common.c \
 targets/cmsis/TARGET_NXP/TARGET_LPC11UXX/system_LPC11Uxx.c \
+targets/cmsis/TARGET_NXP/TARGET_LPC11UXX/cmsis_nvic.c \
 targets/hal/TARGET_NXP/TARGET_LPC11UXX/TARGET_LPC11U24_401/PeripheralPins.c \
 -fdata-sections -ffunction-sections \
 -Wl,--gc-sections \
