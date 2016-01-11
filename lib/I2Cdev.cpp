603892b862c2b9f3c4c258a7183ab58f740a3050
Emily Shepherd <emily@emilyshepherd.me>
2016-01-11 13:39:43 +0000

    Removed unused I2C methods

--- lib/I2Cdev.cpp
+++ lib/I2Cdev.cpp
@@ -2,11 +2,7 @@
 
 I2C i2c(p28, p27);
 
-I2Cdev::I2Cdev(){
-}
-
-int8_t I2Cdev::readByte(uint8_t regAddr, uint8_t *data) {
-    return readBytes(regAddr, 1, data);
+I2Cdev::I2Cdev() {
 }
 
 int8_t I2Cdev::readBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
@@ -18,25 +14,21 @@ int8_t I2Cdev::readBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
 
 bool I2Cdev::writeBit(uint8_t regAddr, uint8_t bitNum, uint8_t data) {
     uint8_t b;
-    readByte(regAddr, &b);
+    readBytes(regAddr, 1, &b);
     b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
-    return writeByte(regAddr, b);
+    return writeBytes(regAddr, 1, &b);
 }
 
 bool I2Cdev::writeBits(uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
     uint8_t b;
-    if(readByte(regAddr, &b))
+    if(readBytes(regAddr, 1, &b))
         return true;
     uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
     data <<= (bitStart - length + 1);
     data &= mask;
     b &= ~(mask);
     b |= data;
-    return writeByte(regAddr, b);
-}
-
-bool I2Cdev::writeByte(uint8_t regAddr, uint8_t data) {
-    return writeBytes(regAddr, 1, &data);
+    return writeBytes(regAddr, 1, &b);
 }
 
 bool I2Cdev::writeBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
--- lib/api/I2Cdev.h
+++ lib/api/I2Cdev.h
@@ -9,12 +9,10 @@ class I2Cdev {
     public:
         I2Cdev();
 
-        static int8_t readByte(uint8_t regAddr, uint8_t *data);
         static int8_t readBytes(uint8_t regAddr, uint8_t length, uint8_t *data);
 
         static bool writeBit(uint8_t regAddr, uint8_t bitNum, uint8_t data);
         static bool writeBits(uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data);
-        static bool writeByte(uint8_t regAddr, uint8_t data);
         static bool writeBytes(uint8_t regAddr, uint8_t length, uint8_t *data);
 };
 
