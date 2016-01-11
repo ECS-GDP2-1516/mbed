cda4ca3553c11df94b8b992188e36f698c00e870
Toby Finch <tobyfinch@googlemail.com>
2016-01-11 18:06:51 +0000

    Add threshold for number of not_ex

--- heuristic.cpp
+++ heuristic.cpp
@@ -3,6 +3,7 @@
 int heur_rear = -1;
 int last_value = -1;
 int heur_buffer[HEUR_SIZE]; //store the latest values from the classifier
+int max_not_ex = 0;
 
 void init_heur() {
     for(int i=0; i<HEUR_SIZE; i++) {
@@ -20,9 +21,13 @@ void insert_value(int value) {
 
 int heur_classify(int value) {
     insert_value(value);
+    int not_ex = 0;
     for(int i=0; i<HEUR_SIZE; i++) {
         if(heur_buffer[i] == NOT_EX) {
-            return 0;
+            not_ex++;
+            if(not_ex > max_not_ex) {
+                return 0;
+            }
         }
     }
     return 1;
