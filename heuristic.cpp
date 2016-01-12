c61fc8fdea46d95cb5334c7c2d83901812e557e3
Emily Shepherd <emily@emilyshepherd.me>
2016-01-12 02:24:35 +0000

    More Inline Code

--- heuristic.cpp
+++ heuristic.cpp
@@ -1,34 +1,3 @@
 #include "heuristic.h"
 
-int8_t heur_rear = -1;
-int8_t last_value = -1;
-uint8_t heur_buffer[HEUR_SIZE]; //store the latest values from the classifier
-uint8_t max_not_ex = 0;
 
-void init_heur() {
-    for(uint8_t i=0; i<HEUR_SIZE; i++) {
-        heur_buffer[i] = NOT_EX;
-    }
-}
-
-void insert_value(uint8_t value) {
-    if(value != last_value) {
-        heur_rear=(heur_rear+1)%HEUR_SIZE;
-        heur_buffer[heur_rear]=value;
-        last_value = value;
-    }
-}
-
-bool heur_classify(uint8_t value) {
-    insert_value(value);
-    uint8_t not_ex = 0;
-    for(uint8_t i=0; i<HEUR_SIZE; i++) {
-        if(heur_buffer[i] == NOT_EX) {
-            not_ex++;
-            if(not_ex > max_not_ex) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
--- heuristic.h
+++ heuristic.h
@@ -1,15 +1,41 @@
 #ifndef _HEURISTIC_H_
 #define _HEURISTIC_H_
 
-#include <stdint.h>
+#include "classify.h"
 
 #define HEUR_SIZE 10
-#define PEAK 0
-#define TROU 1
-#define NOT_EX 2
 
-void init_heur();
+int8_t heur_rear = -1;
+int8_t last_value = -1;
+uint8_t heur_buffer[HEUR_SIZE]; //store the latest values from the classifier
+uint8_t max_not_ex = 0;
 
-bool heur_classify(uint8_t value);
+static inline void init_heur() {
+    for(uint8_t i=0; i<HEUR_SIZE; i++) {
+        heur_buffer[i] = NOT_EX;
+    }
+}
+
+static inline void insert_value(uint8_t value) {
+    if(value != last_value) {
+        heur_rear=(heur_rear+1)%HEUR_SIZE;
+        heur_buffer[heur_rear]=value;
+        last_value = value;
+    }
+}
+
+static inline bool heur_classify(uint8_t value) {
+    insert_value(value);
+    uint8_t not_ex = 0;
+    for(uint8_t i=0; i<HEUR_SIZE; i++) {
+        if(heur_buffer[i] == NOT_EX) {
+            not_ex++;
+            if(not_ex > max_not_ex) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 #endif
