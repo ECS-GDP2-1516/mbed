ECS-GDP2-1516/mbed

commit 65169b61cc611bf715ace6a0fa2243d8de4d6ed1

Fixed reg error in I2C

lib/api/i2c_api.h
@@ -32,6 +32,9 @@
 #define WRITE_ADDR (211 & 0xFE)
 #define READ_ADDR (211 | 0x01)
 
+#define SDA_REG ((__IO uint32_t*)(LPC_IOCON0_BASE + 4 * 5))
+#define SCL_REG ((__IO uint32_t*)(LPC_IOCON0_BASE + 5 * 5))
+
 /**@}*/
 
 enum {
@@ -96,11 +99,13 @@ static inline void i2c_init() {
     i2c_conclr(1, 1, 1, 1);
     i2c_interface_enable();
 
-    __IO uint32_t *regSDA = (__IO uint32_t*)(LPC_IOCON0_BASE + 4 * 5);
-    __IO uint32_t *regSCL = (__IO uint32_t*)(LPC_IOCON0_BASE + 4 * 4);
+    *SDA_REG = (*SDA_REG & ~0x7) | 0x1;
+    uint32_t tmp = *SDA_REG & ~(0x3 << 3) & ~(0x1 << 10);
+    *SDA_REG = tmp;
 
-    *regSDA = ((*regSDA & ~0x7) | 0x1) & ~(0x3 << 3) & ~(0x1 << 10);
-    *regSCL = ((*regSCL & ~0x7) | 0x1) & ~(0x3 << 3) & ~(0x1 << 10);
+    *SCL_REG = (*SCL_REG & ~0x7) | 0x1;
+    tmp = *SCL_REG & ~(0x3 << 3) & ~(0x1 << 10);
+    *SCL_REG = tmp;
 }
 
 static inline int i2c_start() {

commit cf2609583930317878f8d7c9a8105b1c8e4ee797

Fixed second I2C Reg error introduced by 65169b6

lib/api/i2c_api.h
@@ -52,7 +52,7 @@ enum {
 #define READ_ADDR  (211 | 0x01)
 
 #define SDA_REG ((__IO uint32_t*)(LPC_IOCON0_BASE + 4 * 5))
-#define SCL_REG ((__IO uint32_t*)(LPC_IOCON0_BASE + 5 * 5))
+#define SCL_REG ((__IO uint32_t*)(LPC_IOCON0_BASE + 4 * 4))
 
 #define PULSE (__SYSTEM_CLOCK / 200000)
 