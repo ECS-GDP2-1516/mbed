ad72e7c9b7a1ecefefb3b879cc8146bb66a1f2cf
Emily Shepherd <emily@emilyshepherd.me>
2016-01-28 12:19:22 +0000

    Added comments to ASR

--- asr.h
+++ asr.h
@@ -5,6 +5,17 @@
 
 #include <stdint.h>
 
+/**
+ * Software Implementation of Arithmetic Shift Right
+ *
+ * Performs a right shift on a given value, retaining the MSB to
+ * ensure signed integers do not become corrupted. Used to approximate
+ * division by 2 on systems without an ASRS intruction.
+ *
+ * @param int32_t val The value to shift
+ * @param int8_t amount The shift to perform
+ * @return int32_t The shifted value, with negative value preserved
+ */
 static int32_t asr(int32_t val, int8_t amount)
 {
     int32_t msb = val & 0x80;
@@ -14,6 +25,10 @@ static int32_t asr(int32_t val, int8_t amount)
 
 #else
 
+/**
+ * If the device supports ASR, simply use a bitshift and have
+ * GCC create an ASRS instruction.
+ */
 #define asr(x, y)   (x) >> y
 
 #endif
